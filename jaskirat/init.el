68e23dfbbb547bbf56308dd0a9b1eb73c1d9e097
Jaskirat <me@jaskirat.info>
2011-05-11 17:53:07 +0530

    Few changes for org mode

--- jaskirat/init.el
+++ jaskirat/init.el
@@ -1,3 +1,7 @@
 (setq swank-clojure-classpath '("~//.emacs.d/jaskirat/swank-clojure-jars/clojure.jar" "~//.emacs.d/jaskirat/swank-clojure-jars/cloure-contrib.jar" "~//.emacs.d/jaskirat/swank-clojure-jars/swank-clojure.jar"))
 (desktop-save-mode 1)
 
+;; Org-mode yeah!
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+;; Turn on font lock for org-mode buffers only
+(add-hook 'org-mode-hook 'turn-on-font-lock)  
